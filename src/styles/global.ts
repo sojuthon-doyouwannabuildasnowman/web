ef664cacaa77f0c0b82cfc58aa41ffc1d6a615c6
WhiteDog1004 <dlrlgus1204@naver.com>
2022-12-03 15:52:13 +0900

    SNOW-global css font 적용

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,20 +1,22 @@
 import { css } from "@emotion/react";
 
 export const reset = css`
-  * {
-    box-sizing: border-box;
-  }
-  html,
-  body {
-    margin: 0;
-    padding: 0;
-  }
-  html {
-    background-color: #537d93;
-  }
-  body {
-    max-width: 428px;
-    height: 100vh;
-    margin: 0 auto;
-  }
+	* {
+		box-sizing: border-box;
+	}
+	html,
+	body {
+		margin: 0;
+		padding: 0;
+	}
+	html {
+		background-color: #537d93;
+	}
+	body {
+		max-width: 428px;
+		height: 100vh;
+		margin: 0 auto;
+		overflow: hidden;
+		font-family: Pretendard;
+	}
 `;
