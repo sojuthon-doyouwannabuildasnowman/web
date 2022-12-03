973429448d4f1545d641a2962ac416d79fbc5d77
Me1e <mele0404@naver.com>
2022-12-03 11:14:01 +0900

    init

---
+++ pages/api/example.ts
@@ -0,0 +1,23 @@
+import { QueryError } from "mysql2";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../src/common/config/db/db";
+
+type Data = {
+  data: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    db.query("SELECT * FROM test", (err: QueryError | null, result: any) => {
+      if (err) throw err;
+      console.log(result);
+      res.status(200).json({ data: result[0].count });
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ data: "error" });
+  }
+}
---
+++ pages/example.tsx
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function Home({ ssrData }: { ssrData: any }) {
+  const [csrData, setCsrData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        setCsrData(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!ssrData) return <p>SSR data is null</p>;
+
+  return (
+    <div>
+      1: {csrData?.data}
+      <br />
+      2: {ssrData?.data}
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: any) {
+  const res = await fetch(
+    "https://doyouwannabuildasnowman.vercel.app/api/hello"
+    // "http://localhost:3000/api/hello"
+  );
+  const ssrData = await res.json();
+  return {
+    props: {
+      ssrData,
+    },
+  };
+}
