427cfa93ffcf5c211cb6f45401fae3421a13e024
Me1e <mele0404@naver.com>
2022-12-08 13:35:10 +0900

    fix: 날짜 표기

--- pages/info/[id].tsx
+++ pages/info/[id].tsx
@@ -175,7 +175,7 @@ export default function Info() {
         {hours ? (
           <Timer>{hours + " : " + minutes + " : " + seconds}</Timer>
         ) : (
-          <>D-Day 17</>
+          <>D-Day 16</>
         )}
         <Statistics></Statistics>
       </Status>
